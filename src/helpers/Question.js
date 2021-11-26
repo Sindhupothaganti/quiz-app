e2d9d3df0209664bc0beaa0e5dd5adcd8633cbc9
Sindhupothaganti <sindhupothaganti17.com>
2021-11-26 15:54:29 +0530

    quiz app

---
+++ src/helpers/Question.js
@@ -0,0 +1,34 @@
+export const Questions = [
+    {
+      prompt: "What is my name?",
+      optionA: "Sindhu",
+      optionB: "Bindhu",
+      optionC: "Indhu",
+      optionD: "saahi",
+      asnwer: "optionA",
+    },
+    {
+      prompt: "Which of this is not a programming language?",
+      optionA: "Python",
+      optionB: "JavaScript",
+      optionC: "MC-03",
+      optionD: "Java",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      asnwer: "optionD",
+    },
+    {
+        prompt: "Which of this is not a animal?",
+        optionA: "Tiger",
+        optionB: "Lion",
+        optionC: "Monkey",
+        optionD: "parrot",
+        asnwer: "optionD",
+      },
+  ];
